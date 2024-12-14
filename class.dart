f1b05f8f8ea545c741c76bc61deec3da1b9dffb1
 Dawei <100706617+dwhao84@users.noreply.github.com>
2024-12-14 23:30:56 +0800

    Create class.dart

---
+++ class.dart
@@ -0,0 +1,18 @@
+class Dawei {
+  void eat() {
+    print("吃飯🥣");
+  }
+
+  void drinkBubbleTea() {
+    print("喝珍珠奶茶🧋");
+  }
+
+  void sleep() {
+    print("睡覺 🛌");
+  }
+}
+
+void main() {
+  var dawei = Dawei();
+  dawei.sleep();
+}
